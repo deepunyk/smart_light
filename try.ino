1047cdefe1152dedbd3244d5dfc10c0ab9462eb4
deepunyk <39850063+deepunyk@users.noreply.github.com>
2018-06-02 12:01:31 +0530

    Add files via upload

---
+++ try.ino
@@ -0,0 +1,101 @@
+#define RED_PIN   14
+#define GREEN_PIN 12
+#define BLUE_PIN  13
+#define NORMAL_PIN  0
+#define E1 4
+#define E2 5
+#include "config.h"
+
+AdafruitIO_Feed *analog = io.feed("motorcontrol");
+
+
+
+void setup() {
+ 
+pinMode (E1, OUTPUT);
+pinMode (E2, OUTPUT);
+pinMode (NORMAL_PIN, OUTPUT);
+pinMode (BLUE_PIN, OUTPUT);
+pinMode (RED_PIN, OUTPUT);
+pinMode (GREEN_PIN, OUTPUT);
+  digitalWrite(E1, HIGH);
+  digitalWrite(E2, HIGH);
+  Serial.begin(115200);
+  while(! Serial);
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+  analog->onMessage(handleMessage);
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println(io.statusText());
+  analog->get();
+
+ 
+}
+void loop() {
+  io.run();
+  
+}
+void handleMessage(AdafruitIO_Data *data) {
+ int reading = data->toInt();
+  Serial.print("received <- ");
+  Serial.println(reading);
+  if(reading == 16)
+  {
+analogWrite (RED_PIN, 200);
+analogWrite (GREEN_PIN, HIGH);
+digitalWrite (BLUE_PIN, HIGH);
+digitalWrite (NORMAL_PIN, HIGH);
+  }
+   else if(reading == 17)
+  {
+digitalWrite (RED_PIN, HIGH);
+digitalWrite (GREEN_PIN, LOW);
+digitalWrite (BLUE_PIN, HIGH);
+digitalWrite (NORMAL_PIN, HIGH);
+  }
+     else if(reading == 18)
+  {
+digitalWrite (RED_PIN, HIGH);
+digitalWrite (GREEN_PIN, HIGH);
+digitalWrite (BLUE_PIN, LOW);
+digitalWrite (NORMAL_PIN, HIGH);
+  }
+       else if(reading == 20)
+  {
+digitalWrite (RED_PIN, HIGH);
+digitalWrite (GREEN_PIN, LOW);
+digitalWrite (BLUE_PIN, LOW);
+digitalWrite (NORMAL_PIN, HIGH);
+  }
+         else if(reading == 21)
+  {
+digitalWrite (RED_PIN, LOW);
+digitalWrite (GREEN_PIN, HIGH);
+digitalWrite (BLUE_PIN, LOW);
+digitalWrite (NORMAL_PIN, HIGH);
+  }
+           else if(reading == 22)
+  {digitalWrite (RED_PIN, LOW);
+digitalWrite (GREEN_PIN, LOW);
+digitalWrite (BLUE_PIN, HIGH);
+digitalWrite (NORMAL_PIN, HIGH);
+  }         
+  else if(reading == 24)
+  {
+digitalWrite (RED_PIN, LOW);
+digitalWrite (GREEN_PIN, LOW);
+digitalWrite (BLUE_PIN, LOW);
+digitalWrite (NORMAL_PIN, HIGH);
+  }
+  else
+   {
+digitalWrite (RED_PIN, HIGH);
+digitalWrite (GREEN_PIN, HIGH);
+digitalWrite (BLUE_PIN, HIGH);
+digitalWrite (NORMAL_PIN, HIGH);
+  }
+}
